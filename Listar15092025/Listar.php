18882c64867c518d9d4c37e9dcee069470f7a025
daitoros <daviabmedeiros.com>
2025-09-15 09:13:57 -0300

    listar pronto

---
+++ Listar15092025/Listar.php
@@ -0,0 +1,37 @@
+DOCTYPE HTML
+<html>
+    <head>
+        <h1 style="centered">Lista Aluno</h1>
+    </head>
+    <table>
+        <thead>
+            <tr>
+                <th>
+                    <h2>Matricula</h2>
+                </th>
+                <th>
+                    <h2>Nome</h2>
+                </th>
+                <th>
+                    <h2>Email</h2>
+                </th>
+            </tr>
+        </thead>
+</html>
+
+
+<?php
+    $arquivo = fopen("alunos.txt") or die("Impossível criar arquivo!");
+    IF( $arquivo !== false ) {
+        $linha = fgets( $arquivo );
+        while(!feof($arquivo)){
+            $linha = fgets( $arquivo );
+            $dados = explode(";", $linha);
+
+            echo "<tr>";
+            echo "<td>$dados[0]</td>";
+            echo "<td>$dados[1]</td>";
+            echo "<td>$dados[2]</td>";
+            echo "</tr>";
+        }
+?>
