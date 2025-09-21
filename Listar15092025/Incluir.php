8e19d2975675432924817ea0db62b6154a4d0e33
Daitoros <daviabmedeiros@gmail.com>
2025-09-21 18:19:48 -0300

    resolvendo todos os problemas do código!

--- Listar15092025/Incluir.php
+++ Listar15092025/Incluir.php
@@ -1,5 +1,6 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] == 'POST')  {
+if ($_SERVER['REQUEST_METHOD'] == 'POST')  
+{
     $matricula = $_POST["matricula"];
     $nome = $_POST["nome"];
     $email = $_POST["email"];
@@ -10,47 +11,58 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')  {
     $padraoMatricula = '/^\d+$/';
 
 
-    if(!preg_match($padraoEmail, $email)) {
+    if(!preg_match($padraoEmail, $email)) 
+    {
         $msg = "O endereço de email '$email' não é válido!";
-    } else {
-        if(!preg_match($padraoNome, $nome)){
+    } else 
+    {
+        if(!preg_match($padraoNome, $nome))
+        {
             $msg = "O Nome introduzido, '$nome', não é válido!";
-        }else{
-            if(!preg_match($padraoMatricula, $matricula)) {
+        }else
+        {
+            if(!preg_match($padraoMatricula, $matricula)) 
+            {
                 $msg = "A Matrícula '$matricula' não é válida!";
-            }else{
-                if (!file_exists("alunos.txt")) {
+            }else
+            {
+                if (!file_exists("alunos.txt")) 
+                {
                     $arqDisc = fopen("alunos.txt","w") or die("erro ao criar arquivo");
                     $linha = "matricula;nome;email\n";
                     fwrite($arqDisc,$linha);
                     fclose($arqDisc);
                 }
                 $arqDisc = fopen("alunos.txt","a") or die("erro ao abrir arquivo");
-                $linha = $matricula . ";" . $nome . ";" . $email . "\n";
+                $linha = "\n" . $matricula . ";" . $nome . ";" . $email . "\n";
                 fwrite($arqDisc,$linha);
                 fclose($arqDisc);
                 $msg = "Deu tudo certo!!!";
             }
         }
     }
-   
+}   
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-</head>
-<body>
-<h1>Criar Nova Disciplina</h1>
-<form action="Incluir.php" method="POST">
-    Matrícula: <input type="text" name="matricula">
-    <br><br>
-    Nome: <input type="text" name="nome">
-    <br><br>
-    Email: <input type="text" name="email">
-    <br><br>
-    <input type="submit" value="Criar Novo Aluno">
-</form>
-<p><?php echo $msg ?></p>
-<br>
-</body>
+    <head>
+    </head>
+    <body>
+        <h1>Criar Nova Disciplina</h1>
+        <form action="Incluir.php" method="POST">
+            Matrícula: <input type="text" name="matricula">
+            <br><br>
+            Nome: <input type="text" name="nome">
+            <br><br>
+            Email: <input type="text" name="email">
+            <br><br>
+            <input type="submit" value="Criar Novo Aluno">
+        </form>
+        <p><?php 
+            if($_SERVER['REQUEST_METHOD']=='POST'){
+                echo $msg;
+            } 
+        ?></p>
+        <br>
+    </body>
 </html>
--- Listar15092025/Listar.php
+++ Listar15092025/Listar.php
@@ -3,39 +3,42 @@
     <head>
         <h1 style="centered">Lista Aluno</h1>
     </head>
-    <table>
-        <thead>
-            <tr>
-                <th>
-                    <h2>Matricula</h2>
-                </th>
-                <th>
-                    <h2>Nome</h2>
-                </th>
-                <th>
-                    <h2>Email</h2>
-                </th>
-            </tr>
-        </thead>
+    <body>
+        <table>
+            <thead>
+                <tr>
+                    <th>
+                        <h2>Matricula</h2>
+                    </th>
+                    <th>
+                        <h2>Nome</h2>
+                    </th>
+                    <th>
+                        <h2>Email</h2>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                    $arquivo = fopen("alunos.txt", "r") or die("Impossível criar arquivo!");
+                    IF( $arquivo !== false ) {
+                        $linha = fgets($arquivo);
+                        while(($linha = fgets($arquivo))!==false){
+                            // $linha = fgets( $arquivo );
+                            $dados = explode(";", $linha);
+
+                            echo "<tr>";
+                            echo "<td>" . $dados[0] . "</td>";
+                            echo "<td>" . $dados[1] . "</td>";
+                            echo "<td>" . $dados[2] ."</td>";
+                            // echo "<td><a href="alterar.php">Alterar</a></td>";
+                            echo "</tr>";
+                        }
+                        fclose($arquivo);
+                    }
+                    echo "<button><a href='Incluir.php'> Incluir novo aluno</a></button>";    
+                ?>
+            </tbody>
+        <table>
+    </body>
 </html>
-
-
-<?php
-    $arquivo = fopen("alunos.txt") or die("Impossível criar arquivo!");
-    IF( $arquivo !== false ) {
-        $linha = fgets( $arquivo );
-        while(!feof($arquivo)){
-            $linha = fgets( $arquivo );
-            $dados = explode(";", $linha);
-
-            echo "<tr>";
-            echo "<td>$dados[0]</td>";
-            echo "<td>$dados[1]</td>";
-            echo "<td>$dados[2]</td>";
-            // echo "<td><a href="alterar.php">Alterar</a></td>";
-            echo "</tr>";
-        }
-    }
-    echo "<button><a href='Incluir.php'> Incluir nova disciplina</a></button>";    
-?>
-
